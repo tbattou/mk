cf3e84599d457e83ab05017c4eeb375ed7985bc5
tbattou <tbattou@live.com.ar>
2021-12-28 16:06:26 -0300

    segunda entrega, menu

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,14 @@
+import './navBar.css';
+
+export const NavBar = () => {
+    return (
+        <nav className="menu">
+            <ul>
+                <li><a href=''>Remeras</a></li>
+                <li><a href=''>Buzos</a></li>
+                <li><a href=''>Tazas</a></li>
+                <li id='mk'><a href=''>mortalKompra</a></li>
+            </ul>
+        </nav>
+    )
+}
